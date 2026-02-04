4782055450b013bab8b078f4de068bd6f2500e14
vinitasoni1103 <vinitasoni1103@gmail.com>
2026-02-04 16:49:47 +0530

    .

--- Client/src/components/Header.js
+++ Client/src/components/Header.js
@@ -4,7 +4,7 @@ import "./Header.css";
 const Header = () => {
   return (
     <header className="header">
-      <div></div> {/* Empty div to balance flex space */}
+      <div></div> {/* empty div to balance flex space */}
       <h1>3D Model Viewer</h1>
       <Link to="/login" className="login-link">Login</Link>
     </header>
